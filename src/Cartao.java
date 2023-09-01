ecb278b6bbfcd03891f0f8b2373e8dc0b6c3aa6a
TarsilaNascMartins <tarsilawho@gmail.com>
2023-08-31 22:08:58 -0300

    v2 - Herança

---
+++ src/Cartao.java
@@ -0,0 +1,6 @@
+public class Cartao extends Movimentacao {
+
+
+
+
+}
--- src/Movimentacao.java
+++ src/Movimentacao.java
@@ -1,4 +1,46 @@
+import java.util.Scanner;
+
 public class Movimentacao {
+
+
+    int cd_movimentacao;
+    double soma_movimentacao;
+
+    Scanner scc = new Scanner(System.in);
+
+    // Registra o valor
+    public double setMovimentacao(){
+
+        System.out.println("Digite o valor a ser computado:");
+        double movimentacao = scc.nextDouble();
+        cadastrarcd_movimentacao();
+
+        return movimentacao;
+
+    }
+
+    //Faz o acumulo dos valores
+    double somaMovimentacao(double movimentacao){
+        movimentacao += movimentacao;
+
+        return movimentacao;
+    }
+
+
+    //excluir o código pelo id
+    void deletarMovimentacao(int cd_movimentacao){
+
+    }
+
+    Movimentacao getMovimentacao(int cd_movimentacao){
+
+        return null;
+    }
+
+    //incrementa o número do id a cada criação de perfil
+    public void cadastrarcd_movimentacao(){
+        cd_movimentacao++;
+    }
 
 
 
